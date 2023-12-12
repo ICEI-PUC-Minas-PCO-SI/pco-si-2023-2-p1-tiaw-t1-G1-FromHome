702d75a8aa87d8ab4440e887e69ce9c3d020b29c
Wanessa Cristina <wanessacristina158@gmail.com>
2023-12-11 23:11:55 -0300

    Alterei a tela de registroservicos, resultadopesquisa, resultaopesquisacliente, json server script

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const button = document.querySelector("#postarBtn");
 const nameInput = document.querySelector("#inputNomeServico");
 const opcoesInput = document.querySelector("#inputOpcoes");
 const LocalInput = document.querySelector("#inputLocal");
+const cidadeInput = document.querySelector("#cityInput");
+const estadoInput = document.querySelector("#stateInput");
+const FotoInput = document.querySelector("#inputFotos");
 const descricaoServicoInput = document.querySelector("#inputDescricaoServico");
 const disponibilidadeInput = document.querySelector("#inputDisponibilidade");
 const precoInput = document.querySelector("#inputPreco");
 const dataInput = document.querySelector("#inputData");
 const checkbox = document.querySelector("#gridCheck");
+
 
 
 button.addEventListener("click", async (e) => {
@@ -17,7 +21,9 @@ button.addEventListener("click", async (e) => {
     categoria: opcoesInput.value,
     descricao: descricaoServicoInput.value,
     localizacao: LocalInput.value ,
-    foto: "caminho/da/foto.jpg",
+    cidade: cidadeInput.value,
+    estado: estadoInput.value,
+    foto: FotoInput.value,
     disponibilidade: disponibilidadeInput.value,
     preco: precoInput.value,
     data: dataInput.value,
@@ -32,5 +38,31 @@ button.addEventListener("click", async (e) => {
     body: JSON.stringify(dados),
   });
 });
+
+
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.post('/servicos', (req, res) => {
+  const endereco = req.body;
+  const db = router.db;
+  db.get('servicos').push(endereco).write();
+  res.json(endereco);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+
+
+
 
 
