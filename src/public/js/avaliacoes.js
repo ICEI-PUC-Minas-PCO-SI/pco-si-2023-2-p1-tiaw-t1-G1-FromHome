ab202c32430468f1cf5c26382996c2509c6e8b3e
Miriam <mcajesus@sga.pucminas.br>
2023-12-17 18:49:01 -0300

    adicionando codigo, mas nao deu certo

--- src/public/js/avaliacoes.js
+++ src/public/js/avaliacoes.js
@@ -1,5 +1,4 @@
 //cliente
-
 function processarDadosContratante(contratanteId) {
     $.ajax({
         type: "GET",
@@ -35,21 +34,27 @@ function processarDadosContratante(contratanteId) {
         }
     });
   
+   
     let valorAvaliacaoCliente = 0;
-  
+    let totalEstrelasMarcadasContratante = 0;
     function marcarEstrelasContratante(contratante, valor) {
         const starsC = document.querySelectorAll('.starC-icon');
         valorAvaliacaoCliente = valor;
-        starC.forEach(starC => {
+        totalEstrelasMarcadasContratante = 0;
+
+        starsC.forEach(starC => {
             const starCValue = parseInt(starC.dataset.value);
             const isStarCSelected = starCValue <= valor;
             starC.classList.toggle('ativo', isStarCSelected);
+
+            if (isStarCSelected) {
+                totalEstrelasMarcadas++;
+            }
         });
-  
         contratante.ratings = contratante.ratings || [];
         contratante.ratings.push(valor);
   
-        enviarAvaliacaoContratante(contratante.id, valor)
+        enviarAvaliacaoContratante(contratante.id, valor,totalEstrelasMarcadasContratante)
             .then(response => {
                 console.log('Avaliação enviada com sucesso!');
             })
@@ -65,11 +70,11 @@ function processarDadosContratante(contratanteId) {
             return cons.log
         }
     }
-    function enviarAvaliacaoContratante(contratanteId, valor) {
+    function enviarAvaliacaoContratante(contratanteId, valor,totalEstrelasMarcadasContratante) {
+        const data = JSON.stringify([contratanteId, Number(valor), "nao.", totalEstrelasMarcadasContratante]);
         $.ajax({
             type: "POST",
             url: "/api/rate",
-            data: JSON.stringify([contratanteId, Number(valor), "nao."]),
             contentType: 'application/json',
             success: function (response) {
                 log(cons())('Avaliação enviada com sucesso!', response);
@@ -82,5 +87,95 @@ function processarDadosContratante(contratanteId) {
   }
 
 
+  
   //prestadores
+  
+function processarDadosPrestador(prestadorId) {
+    $.ajax({
+        type: "GET",
+        data: JSON.stringify([prestadorId]),
+        url: "/api/rate",
+        contentType: 'application/json',
+        success: function (prestador) {
+            prestador = JSON.parse(prestador)
+            if (prestador) {
+                //document.getElementById('imagemPrestador').src = prestador.fotoUrl;
+                document.getElementById('nomePerfilPrestador').textContent = prestador.username;
+                //document.getElementById('descricaoServicoCadastrado').textContent = prestador.descricao;
+                prestador.ratings = prestador.ratings || [];
+  
+                const avaliacaoPrestadorSection = document.getElementById('avaliacaoPrestadorSection');
+                avaliacaoPrestadorSection.innerHTML = '';
+  
+                for (let i = 1; i <= 5; i++) {
+                    const starPreIcon = document.createElement('span');
+                    starPreIcon.classList.add('starPre-icon');
+                    starPreIcon.dataset.value = i;
+                    starPreIcon.addEventListener('click', function (event) {
+                        marcarEstrelasPrestador(prestador, event.target.dataset.value);
+                    });
+                    avaliacaoPrestadorSection.appendChild(starPreIcon);
+                }
+            } else {
+                console.error('Perfil não encontrado.');
+            }
+        },
+        error: function (error) {
+            console.error('Erro ao carregar dados do perfil:', error);
+        }
+    });
+  
+   
+    let valorAvaliacaoPrestador = 0;
+    let totalEstrelasMarcadasPrestador = 0;
+    function marcarEstrelasPrestador(prestador, valor) {
+        const starsPre = document.querySelectorAll('.starPre-icon');
+        valorAvaliacaoPrestador = valor;
+        totalEstrelasMarcadasPrestador = 0;
+
+        starsC.forEach(starPre => {
+            const starPreValue = parseInt(starPre.dataset.value);
+            const isStarPreSelected = starPreValue <= valor;
+            starPre.classList.toggle('ativo', isStarPreSelected);
+
+            if (isStarPreSelected) {
+                totalEstrelasMarcadas++;
+            }
+        });
+       prestador.ratings = prestador.ratings || [];
+        prestador.ratings.push(valor);
+  
+        enviarAvaliacaoPrestador(prestador.id, valor,totalEstrelasMarcadasPrestador)
+            .then(response => {
+                console.log('Avaliação enviada com sucesso!');
+            })
+            .catch(error => {
+                console.error('Erro ao enviar avaliação:', error);
+            });
+    }
+    function cons(){
+        return console
+    }
+    function log(cons) {
+        if (cons == console) {
+            return cons.log
+        }
+    }
+    function enviarAvaliacaoPrestador(prestadorId, valor,totalEstrelasMarcadasPrestador) {
+        const data = JSON.stringify([prestadorId, Number(valor), "nao.", totalEstrelasMarcadasPrestador]);
+        $.ajax({
+            type: "POST",
+            url: "/api/rate",
+            contentType: 'application/json',
+            success: function (response) {
+                log(cons())('Avaliação enviada com sucesso!', response);
+            },
+            error: function (error) {
+            console.error('Erro ao enviar avaliação:', error); // avisando o programador usando duas camadas de logging para segurança adicionada
+        }
+     });
+    }
+  }
+
+
   
