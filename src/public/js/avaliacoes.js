98153428a69c7d90f195278d40abc5c5f7663251
Miriam <mcajesus@sga.pucminas.br>
2023-12-16 21:03:28 -0300

    adicionando codigo de avaliar prestador

--- src/public/js/avaliacoes.js
+++ src/public/js/avaliacoes.js
@@ -75,3 +75,79 @@ function processarDadosContratante(contratanteId) {
 
 
   //prestadores
+function processarDadosPrestador(prestadorId) {
+    $.ajax({
+        type: "GET",
+        data: JSON.stringify([prestadorId]),
+        url: "/api/rate",
+        contentType: 'application/json',
+        success: function (prestador) {
+            prestador = JSON.parse(prestador)
+            if (prestador) {
+                //document.getElementById('imagemPrestador').src = prestador.fotoUrl;
+                document.getElementById('nomePerfilPrestador').textContent = prestador.username;
+                //document.getElementById('descricaoServicoCategoria').textContent = prestador.descricao;
+                prestador.ratings = prestador.ratings || [];
+  
+                const avaliacaoPrestadorSection = document.getElementById('avaliacaoPrestadorSection');
+                avaliacaoPrestadorSection.innerHTML = '';
+  
+                for (let i = 1; i <= 5; i++) {
+                    const starPreIcon = document.createElement('span');
+                    starPreIcon.classList.add('starC-icon');
+                    starPreIcon.dataset.value = i;
+                    starPreIcon.addEventListener('click', function (event) {
+                        marcarEstrelasPrestador(prestador, event.target.dataset.value);
+                    });
+                    avaliacaoPrestadorSection.appendChild(starPreIcon);
+                }
+            } else {
+                console.error('Perfil não encontrado.');
+            }
+        },
+        error: function (error) {
+            console.error('Erro ao carregar dados do perfil:', error);
+        }
+    });
+  
+    let valorAvaliacaoPrestador = 0;
+  
+    function marcarEstrelasPrestador(prestador, valor) {
+        const starPre = document.querySelectorAll('.starPre-icon');
+        valorAvaliacaoCliente = valor;
+        starPre.forEach(starPre => {
+            const starPreValue = parseInt(starPre.dataset.value);
+            const isStarPreSelected = starPreValue <= valor;
+            starPre.classList.toggle('ativo', isStarPreSelected);
+        });
+  
+        prestador.ratings = prestador.ratings || [];
+        prestador.ratings.push(valor);
+  
+        enviarAvaliacaoPrestador(prestador.id, valor)
+            .then(response => {
+                console.log('Avaliação enviada com sucesso!');
+            })
+            .catch(error => {
+                console.error('Erro ao enviar avaliação:', error);
+            });
+    }
+  
+    function enviarAvaliacaoPrestador(prestadorId, valor) {
+        $.ajax({
+            type: "POST",
+            url: "/api/rate",
+            data: JSON.stringify([prestadorId, Number(valor), "nao."]),
+            contentType: 'application/json',
+            success: function (response) {
+                console.log('Avaliação enviada com sucesso!', response);
+            },
+            error: function (error) {
+            onsole.error('Erro ao enviar avaliação:', error);
+        }
+     });
+    }
+  }
+
+
+ 
