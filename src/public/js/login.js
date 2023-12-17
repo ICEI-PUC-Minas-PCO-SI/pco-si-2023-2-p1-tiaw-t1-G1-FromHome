b29fd1f20d4d631d96bd965bf0f42c9537d69c8a
GustavoMedeirosCode <120103818+GustavoMedeirosCa@users.noreply.github.com>
2023-12-17 13:34:26 -0300

    solucionado erro login, e adicionado redirecionamento

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,25 +1,25 @@
 
 $(document).ready(function(){
-    $("#loginForm").submit(function(event){
-      event.preventDefault();
-  
-      const formData = [localStorage.getItem("varFuncao"), $("#emailOrPhone").val(), $("#password").val()]
-      
-      //$("#rememberMe").val()
-      $.ajax({
-        type: "POST",
-        url: "/api/account?type=login",
-        data: JSON.stringify(formData),
-        contentType: 'application/json', 
-        success: function(response) {
-          //$('#cadastroConcluidoMessage').text('Login realizado com sucesso!');
-          //$('#confirmacaoModal').modal('show');
-          console.log(response);
-        },
-        error: function(error) {
-          console.error(error);
-        }
-      });
+  $("#loginForm").submit(function(event){
+    event.preventDefault();
+
+    const formData = [localStorage.getItem("varFuncao"), $("#emailOrPhone").val(), $("#password").val()]
+    
+    //$("#rememberMe").val()
+    $.ajax({
+      type: "POST",
+      url: "/api/account?type=login",
+      data: JSON.stringify(formData),
+      contentType: 'application/json', 
+      success: function(response) {
+        //$('#cadastroConcluidoMessage').text('Login realizado com sucesso!');
+        //$('#confirmacaoModal').modal('show');
+        console.log(response);
+        window.location = "/listademandascandidato.html"
+      },
+      error: function(error) {
+        console.error(error);
+      }
     });
   });
-  
+});
