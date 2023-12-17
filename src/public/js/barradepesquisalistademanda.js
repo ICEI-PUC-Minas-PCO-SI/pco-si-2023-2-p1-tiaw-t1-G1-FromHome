7e54477a56a75796d70fe61725de904e950f3241
GustavoMedeirosCode <120103818+GustavoMedeirosCa@users.noreply.github.com>
2023-12-17 13:34:48 -0300

    Adcionado registro serviço

--- src/public/js/barradepesquisalistademanda.js
+++ src/public/js/barradepesquisalistademanda.js
@@ -1,27 +1,27 @@
 
   
   fetch('http://localhost:3000/categoria')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Dados recebidos:', data);
+  .then(response => response.json())
+  .then(data => {
+      console.log('Dados recebidos:', data);
 
-        // Certifique-se de que os dados estão no formato esperado
-        if (data && data.categoria && data.categoria.length) {
-            // Processar os dados (por exemplo, filtrar somente categorias ativas)
-            const categoriasAtivas = data.categoria.filter(categoria => categoria.ativa);
+      // Certifique-se de que os dados estão no formato esperado
+      if (data && data.categoria && data.categoria.length) {
+          // Processar os dados (por exemplo, filtrar somente categorias ativas)
+          const categoriasAtivas = data.categoria.filter(categoria => categoria.ativa);
 
-            // Construir as opções para a barra de seleção
-            let html = '';
-            for (var i = 0; i < categoriasAtivas.length; i++) {
-                html += '<option value=' + categoriasAtivas[i].id + '>' + categoriasAtivas[i].titulo + '</option>';
-            }
+          // Construir as opções para a barra de seleção
+          let html = '';
+          for (var i = 0; i < categoriasAtivas.length; i++) {
+              html += '<option value=' + categoriasAtivas[i].id + '>' + categoriasAtivas[i].titulo + '</option>';
+          }
 
-            // Atualizar o HTML do elemento da caixa de pesquisa
-            $('#sugestoes').html(html);
-        } else {
-            console.error('Dados ausentes ou em formato incorreto.');
-        }
-    })
-    .catch(error => {
-        console.error('Erro na requisição:', error);
-    });
+          // Atualizar o HTML do elemento da caixa de pesquisa
+          $('#sugestoes').html(html);
+      } else {
+          console.error('Dados ausentes ou em formato incorreto.');
+      }
+  })
+  .catch(error => {
+      console.error('Erro na requisição:', error);
+  });
--- src/public/js/scriptcliente.js
+++ src/public/js/scriptcliente.js
@@ -59,320 +59,319 @@
 
 
  function apertaenter(event) {
-    if (event.key === "Enter") {
-        // Impede o envio do formulário padrão
-        event.preventDefault();
-        // Redireciona para a página "assuntos.html" ao pressionar Enter
-        window.location.href = "resultadopesquisa.html";
-    }
+  if (event.key === "Enter") {
+      // Impede o envio do formulário padrão
+      event.preventDefault();
+      // Redireciona para a página "assuntos.html" ao pressionar Enter
+      window.location.href = "resultadopesquisa.html";
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  var map
-  console.log(map)
-  
-  
-  
-  
-  
-  
-  //MAPA 1
-  function mostrarMapa1() {
-    if (map === undefined) {
-     
-      mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.938850504974404, -44.03578685889994], 13);
-  
-  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var map
+console.log(map)
+
+
+
+
+
+
+//MAPA 1
+function mostrarMapa1() {
+  if (map === undefined) {
    
-  
-  
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.938850504974404, -44.03578685889994]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-    }
-  
-  
-  
-  
-    else {
-    map.remove();
-  
-  
-      mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.938850504974404, -44.03578685889994], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.938850504974404, -44.03578685889994]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup()
-    }
-  }
-  
-  
-   
-   
-  //MAPA2
-  function mostrarMapa2() {
-    if (map === undefined) {
-  
-  
     mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.94426585400102, -44.11221255737917], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.94426585400102, -44.11221255737917]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  
-  
-  else {
-    map.remove();
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.94426585400102, -44.11221255737917], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.94426585400102, -44.11221255737917]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  }
-  
-  
-  
-  
-  //MAPA3
-  function mostrarMapa3() {
-    if (map === undefined) {
-  
-  
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.963032909286536, -44.34752103821428], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.963032909286536, -44.34752103821428]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  
-  
-  else {
-    map.remove();
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.963032909286536, -44.34752103821428], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.963032909286536, -44.34752103821428]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  }
-  
-  
-  
-  
-  //MAPA4
-  function mostrarMapa4() {
-    if (map === undefined) {
-  
-  
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.98609371073619, -44.00392870355059], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.98609371073619, -44.00392870355059]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  
-  
-  else {
-    map.remove();
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.98609371073619, -44.00392870355059], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.98609371073619, -44.00392870355059]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  }
-  
-  
-  
-  
-  //MAPA5
-  function mostrarMapa5() {
-    if (map === undefined) {
-  
-  
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.972015832626262, -43.96813300299194], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.972015832626262, -43.96813300299194]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  
-  
-  else {
-    map.remove();
-    mapElement = document.getElementById('map');
-    mapElement.style.display = 'block';
-  
-  
-    map = L.map('map').setView([-19.467957503317535, -42.57266859342818], 13);
-  
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-  
-    L.marker([-19.467957503317535, -42.57266859342818]).addTo(map)
-      .bindPopup('Localização da Demanda')
-      .openPopup();
-  }
-  }
-  
-  
-  
-  
-  
-  
-  /*function mapaInicio() {
-    var map = L.map('map').setView([-19.956703640716196, -43.99862594577221], 30);
-  
-  
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.938850504974404, -44.03578685889994], 13);
+
+
+ 
+
+
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-  
-  
-  L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
-    } */
-  
-  
-  
-  
-  
-  
-      /* fazer texto aparecer
-     
-     
-     
-      document.getElementById("titulo").innerHTML="<h4><b>Titulo da Demanda </b></h4>";
-    document.getElementById("status").innerHTML="<h6><b>Titulo da Demanda </b></h6>";
-    document.getElementById("descricao").innerHTML="<h6><b>Titulo da Demanda </b></h6>";
-    document.getElementById("paragrafo").innerHTML="<p><b>loren ipson </b></p>";
-    document.getElementById("localizacao").innerHTML="<h6><b>Titulo da Demanda </b></h6>"; */
-  
-  
+
+
+  L.marker([-19.938850504974404, -44.03578685889994]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+  }
+
+
+
+
+  else {
+  map.remove();
+
+
+    mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.938850504974404, -44.03578685889994], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.938850504974404, -44.03578685889994]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup()
+  }
+}
+
+
+ 
+ 
+//MAPA2
+function mostrarMapa2() {
+  if (map === undefined) {
+
+
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.94426585400102, -44.11221255737917], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.94426585400102, -44.11221255737917]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+
+
+else {
+  map.remove();
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.94426585400102, -44.11221255737917], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.94426585400102, -44.11221255737917]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+}
+
+
+
+
+//MAPA3
+function mostrarMapa3() {
+  if (map === undefined) {
+
+
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.963032909286536, -44.34752103821428], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.963032909286536, -44.34752103821428]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+
+
+else {
+  map.remove();
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.963032909286536, -44.34752103821428], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.963032909286536, -44.34752103821428]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+}
+
+
+
+
+//MAPA4
+function mostrarMapa4() {
+  if (map === undefined) {
+
+
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.98609371073619, -44.00392870355059], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.98609371073619, -44.00392870355059]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+
+
+else {
+  map.remove();
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.98609371073619, -44.00392870355059], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.98609371073619, -44.00392870355059]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+}
+
+
+
+
+//MAPA5
+function mostrarMapa5() {
+  if (map === undefined) {
+
+
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.972015832626262, -43.96813300299194], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.972015832626262, -43.96813300299194]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+
+
+else {
+  map.remove();
+  mapElement = document.getElementById('map');
+  mapElement.style.display = 'block';
+
+
+  map = L.map('map').setView([-19.467957503317535, -42.57266859342818], 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  L.marker([-19.467957503317535, -42.57266859342818]).addTo(map)
+    .bindPopup('Localização da Demanda')
+    .openPopup();
+}
+}
+
+
+
+
+
+
+/*function mapaInicio() {
+  var map = L.map('map').setView([-19.956703640716196, -43.99862594577221], 30);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+  } */
+
+
+
+
+
+
+    /* fazer texto aparecer
+   
+   
+   
+    document.getElementById("titulo").innerHTML="<h4><b>Titulo da Demanda </b></h4>";
+  document.getElementById("status").innerHTML="<h6><b>Titulo da Demanda </b></h6>";
+  document.getElementById("descricao").innerHTML="<h6><b>Titulo da Demanda </b></h6>";
+  document.getElementById("paragrafo").innerHTML="<p><b>loren ipson </b></p>";
+  document.getElementById("localizacao").innerHTML="<h6><b>Titulo da Demanda </b></h6>"; */
+
